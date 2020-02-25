b64cd60f388ebd864a591d93bccd89bc162a0152
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-02-25 15:37:14 -0600

    added JavaScript questions and more CSS styling

--- js/app.js
+++ js/app.js
@@ -1,3 +1,69 @@
 'use strict';
 
 console.log('ready to rock');
+
+var name = prompt('Hello! Who are you?');
+
+alert('Hello, ' + name + '. Thanks for visiting!');
+
+alert('Please, answer "yes" or "no" to the following questions.');
+
+var study = prompt('Did I study cooking and food science?').toLowerCase();
+
+if(study === 'yes') {
+    alert('You are right. For one year out of high school. Good work!');
+} else if(study === 'no') {
+    alert('No. Not right. Thanks for trying, though.');
+} else {
+    alert('Please enter either "yes" or "no."');
+}
+
+console.log(study);
+
+var dogs = prompt('Do I own 3 dogs?').toLowerCase();
+
+if(dogs === 'yes') {
+    alert('Good guess, but wrong.');
+} else if(dogs === 'no') {
+    alert('Correct! I have neither the time nor the money.');
+} else {
+    alert('Please enter either "yes" or "no."');
+}
+
+console.log(dogs);
+
+var deltaV = prompt('Am I a Delta V student?').toLowerCase();
+
+if(deltaV === 'yes') {
+    alert('Yes! And Iowa\'s first Digital Marketing Apprentice.');
+} else if(deltaV === 'no') {
+    alert('Wrong. But I know you can do better!');
+} else {
+    alert('Please enter either "yes" or "no."');
+}
+
+console.log(deltaV);
+
+var fastFood = prompt('Have I managed a fast food restaurant?').toLowerCase();
+
+if(fastFood === 'yes') {
+    alert('Yep, at 3 different Burger Kings in Omaha, Nebraska.');
+} else if(fastFood === 'no') {
+    alert('Incorrect.');
+} else {
+    alert('Please enter either "yes" or "no."');
+}
+
+console.log(fastFood);
+
+var sopes = prompt('Do I enjoy making sopes?').toLowerCase();
+
+if(sopes === 'yes') {
+    alert('You are right. A solid guess.');
+} else if(sopes === 'no') {
+    alert('No, I  really do.');
+} else {
+    alert('Please enter either "yes" or "no."');
+}
+
+console.log(sopes);
