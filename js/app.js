3e08d537aaec38767c72cd96fb1a2a137906e67f
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-03-01 22:54:04 -0600

    Cleaned out console.logs and comments

--- js/app.js
+++ js/app.js
@@ -8,55 +8,50 @@ var arrayT = ['You are right. For one year out of high school. Good work!', 'Cor
 var arrayF = ['No. Not right. Thanks for trying, though.', 'Good guess, but wrong.', 'Wrong. But I know you can do better!', 'Incorrect.', 'No, I really do.', 'Please answer "yes" or "no".'];
 var arrayA = ['yes', 'no', 'yes', 'yes', 'yes'];
 var arrayD = ['charlie', 'gideon', 'abby'];
-var userPoints = 0
+var userPoints = 0;
 var count = 0;
-var randomNumber = Math.ceil(Math.random()*10);
-console.log('random answer: ' + randomNumber);
+var randomNumber = Math.ceil(Math.random() * 10);
 
 ////////////////////functions////////////////////////
 function greeting() {
-var name = prompt('Hello! Who are you?');
-alert('Hello, ' + name + '. Thanks for visiting!');
+  var name = prompt('Hello! Who are you?');
+  alert('Hello, ' + name + '. Thanks for visiting!');
 }
 greeting();
 
-
 function quiz() {
 
-alert('Please, answer "yes" or "no" to the following questions.');
+  alert('Please, answer "yes" or "no" to the following questions.');
 
-for(var i=0; i<5; i++) {
-    console.log(i);
+  for (var i = 0; i < 5; i++) {
     var answer = prompt(arrayQ[i]).toLowerCase();
-    console.log(arrayQ[i] + ' = ' + answer);
-    if(answer !== 'yes' && answer !== 'no'){
-        alert(arrayF[5]);
-        i--;
-    }else if(answer === arrayA[i]) {
-        alert(arrayT[i]);
-        userPoints++;
-        console.log('points: ' + userPoints);
-    }else{alert(arrayF[i]);}
-}
+    if (answer !== 'yes' && answer !== 'no') {
+      alert(arrayF[5]);
+      i--;
+    } else if (answer === arrayA[i]) {
+      alert(arrayT[i]);
+      userPoints++;
+    } else { alert(arrayF[i]); }
+  }
 
-for (count = 0; count<4; count++) {
-    console.log('count:' + count);
+  for (count = 0; count < 4; count++) {
     var guess = parseInt(prompt('Can you guess what number between 1 and 10 I\'m thinking of? Give it a go!'));
-    if (guess > randomNumber) {alert('Too high, my friend!');} else if (guess < randomNumber){alert('Too low, my friend!');} else if(guess === randomNumber){alert('Nailed it!');
-        userPoints++;
-        console.log('points: ' + userPoints);
-        break;
-    }else {alert('Please, enter a number!');}
-}
-for(var j = 0; j<1; j++) {
+    if (guess > randomNumber) { alert('Too high, my friend!'); } else if (guess < randomNumber) { alert('Too low, my friend!'); } else if (guess === randomNumber) {
+      alert('Nailed it!');
+      userPoints++;
+      break;
+    } else { alert('Please, enter a number!'); }
+  }
+  for (var j = 0; j < 1; j++) {
     var answerD = prompt('Which of the following is the name of a dog I have raised?\n Charlie, Moose, Gideon, Abby, Bones, Zack, Scruff McGruff, or Linkin Bark').toLowerCase();
-    if(answerD === arrayD[0] || answerD === arrayD[1] || answerD === arrayD[2]){alert('Good work!');
-        userPoints++;
-        console.log('points: ' + userPoints);}else{alert('Wrong, but good try!');}
+    if (answerD === arrayD[0] || answerD === arrayD[1] || answerD === arrayD[2]) {
+      alert('Good work!');
+      userPoints++;
+    } else { alert('Wrong, but good try!'); }
+  }
+
+
+
+  alert(userPoints + ' points! Thanks for playing!');
 }
 
-
-
-alert(userPoints + ' points! Thanks for playing, ' + name + '!');
-}
-quiz();
